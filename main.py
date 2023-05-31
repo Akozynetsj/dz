987541ea7b4ae486ac02646eaa2bad36eee7acf2
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 20:55:13 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def is_palindrome(string):
-    string = string.replace(" ", "").lower()
-    reversed_string = string[::-1]
-    if string == reversed_string:
-        return True
-    else:
-        return False
-str1 = "А роза упала на лапу Азора"
-print(f'Рядок "{str1}" є паліндромом: {is_palindrome(str1)}')
-str2 = "Hello, World!"
-print(f'Рядок "{str2}" є паліндромом: {is_palindrome(str2)}')
-
+def sort_strings_by_length(string_list):
+    return sorted(string_list, key=len)
+strings = ["apple", "banana", "cherry", "date", "elderberry"]
+sorted_strings = sort_strings_by_length(strings)
+print(f'Відсортований список рядків: {sorted_strings}')
