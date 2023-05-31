c405a109bb33cbab7fc959b87b5baa0d464bc234
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 20:45:08 +0300

    ex 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def sum_of_list_elements(lst):
+    return sum(lst)
+
+# Приклад використання функції
+list1 = [1, 2, 3, 4, 5]
+sum1 = sum_of_list_elements(list1)
+print(f"Сума елементів списку {list1} дорівнює {sum1}")
+
+list2 = [10, 20, 30, 40, 50]
+sum2 = sum_of_list_elements(list2)
+print(f"Сума елементів списку {list2} дорівнює {sum2}")
 
