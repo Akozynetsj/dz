9b0ac1c2d6d849fe62a31b43ebac96235aab67b3
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 20:53:24 +0300

    ex 2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def sum_of_list_elements(lst):
-    return sum(lst)
-
-# Приклад використання функції
-list1 = [1, 2, 3, 4, 5]
-sum1 = sum_of_list_elements(list1)
-print(f"Сума елементів списку {list1} дорівнює {sum1}")
-
-list2 = [10, 20, 30, 40, 50]
-sum2 = sum_of_list_elements(list2)
-print(f"Сума елементів списку {list2} дорівнює {sum2}")
+def is_palindrome(string):
+    string = string.replace(" ", "").lower()
+    reversed_string = string[::-1]
+    if string == reversed_string:
+        return True
+    else:
+        return False
+str1 = "А роза упала на лапу Азора"
+print(f'Рядок "{str1}" є паліндромом: {is_palindrome(str1)}')
+str2 = "Hello, World!"
+print(f'Рядок "{str2}" є паліндромом: {is_palindrome(str2)}')
 
